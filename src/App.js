1f851a2bb489e37a8d44edeb41b5c02713730969
shresthakhya <shresthakhya7@gmail.com>
2024-06-02 14:35:54 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-
 const App = () => {
   return (
     <div className="App">
